c855bf4dedffc13c9bb917c5c852410cbc8cce01
Maksim <s1mplexgod1337@gmail.com>
2022-12-09 12:29:44 +0200

    finish html and css and initial js

---
+++ script.js
@@ -0,0 +1,7 @@
+const shortBreak = document.getElementById('short')
+const longBrear = document.getElementById('long')
+const pomodoro = document.getElementById('pomodoro')
+const button = document.getElementById('button')
+const pomodoroText = document.getElementById('text')
+
+pomodoroText.textContent = '25:00'

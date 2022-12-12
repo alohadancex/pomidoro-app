c2e30935dfb6c8e6e7a01e7acc112643a92cfab6
Maksim <s1mplexgod1337@gmail.com>
2022-12-12 17:43:56 +0200

    restart all project

--- script.js
+++ script.js
@@ -1,59 +1,20 @@
-const shortBreak = document.getElementById('short')
-const longBreak = document.getElementById('long')
-const button = document.getElementById('button')
-const pomodoro = document.getElementById('pomodoro')
-const minute = document.getElementById('minute')
-const second = document.getElementById('second')
+const mainButton = document.getElementById('js-btn')
+const modeButtons = document.querySelector('#js-mode-buttons')
 
-let workTime = 25
-let shortTime = 5
-let longTime = 15
-
-let seconds = '00'
-
-minute.innerText = workTime
-second.innerText = seconds
+const timer = {
+	pomodoro: 25,
+	shortBreak: 5,
+	longBreak: 15,
+	longBreakInterval: 4,
+}
 
 let intervalId
 
-function start() {
-	document.getElementById('start').style.display = 'none'
-	document.getElementById('stop').style.display = 'initial'
+modeButtons.addEventListener('click', handleMode)
 
-	seconds = 59
+function handleMode(e) {
+	const { mode } = e.target.dataset
 
-	let workMinutes = workTime - 1
-	let breakMinutes = shortTime - 1
-
-	breakCount = 0
-
-	let timerFunction = () => {
-		minute.innerText = workMinutes
-		second.innerText = seconds
-
-		seconds = seconds - 1
-
-		if (seconds === 0) {
-			workMinutes = workMinutes - 1
-			if (workMinutes === -1) {
-				if (breakCount % 2 === 0) {
-					// start break
-
-					workMinutes = breakMinutes
-					breakCount++
-				} else {
-					workMinutes = workTime
-					breakCount++
-				}
-			}
-			seconds = 59
-		}
-	}
-	intervalId = setInterval(timerFunction, 1000)
-}
-
-function stopInterval() {
-	clearInterval(intervalId)
-	document.getElementById('start').style.display = 'initial'
-	document.getElementById('stop').style.display = 'none'
+	if (!mode) return
+	switchMode(mode)
 }

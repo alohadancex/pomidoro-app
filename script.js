72ac5eba856e2ad0f353efdec1cc366ade679fae
Maksim <s1mplexgod1337@gmail.com>
2022-12-13 12:24:03 +0200

    creat function for update timer content

--- script.js
+++ script.js
@@ -14,7 +14,37 @@ modeButtons.addEventListener('click', handleMode)
 
 function handleMode(e) {
 	const { mode } = e.target.dataset
+	console.log(mode)
 
 	if (!mode) return
 	switchMode(mode)
+}
+
+function switchMode(mode) {
+	timer.mode = mode
+	timer.remainingTime = {
+		total: timer[mode] * 60,
+		minutes: timer[mode],
+		seconds: 0,
+	}
+
+	document
+		.querySelectorAll('button[data-mode]')
+		.forEach(e => e.classList.remove('active'))
+	document.querySelector(`[data-mode="${mode}"]`).classList.add('active')
+
+	updateClock()
+}
+
+function updateClock() {
+	const { remainingTime } = timer
+	console.log(remainingTime)
+
+	const minutes = `${remainingTime.minutes}`.padStart(2, '0')
+	const seconds = `${remainingTime.seconds}`.padStart(2, '0')
+
+	const min = document.getElementById('js-minutes')
+	const sec = document.getElementById('js-seconds')
+	min.textContent = minutes
+	sec.textContent = seconds
 }

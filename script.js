a71abce917610a1d93dc2ebf787ce857421a3ffb
Maksim <s1mplexgod1337@gmail.com>
2022-12-13 13:47:30 +0200

    add update, change, progress, stopTimer, startTim

--- script.js
+++ script.js
@@ -1,23 +1,94 @@
-const mainButton = document.getElementById('js-btn')
-const modeButtons = document.querySelector('#js-mode-buttons')
-
 const timer = {
 	pomodoro: 25,
 	shortBreak: 5,
 	longBreak: 15,
 	longBreakInterval: 4,
+	sessions: 0,
 }
 
-let intervalId
+let interval
 
+const mainButton = document.getElementById('js-btn')
+mainButton.addEventListener('click', () => {
+	const { action } = mainButton.dataset
+	if (action === 'start') {
+		startTimer()
+	} else {
+		stopTimer()
+	}
+})
+
+const modeButtons = document.querySelector('#js-mode-buttons')
 modeButtons.addEventListener('click', handleMode)
 
-function handleMode(e) {
-	const { mode } = e.target.dataset
-	console.log(mode)
+function getRemainingTime(endTime) {
+	const currentTime = Date.parse(new Date())
+	const difference = endTime - currentTime
 
-	if (!mode) return
-	switchMode(mode)
+	const total = Number.parseInt(difference / 1000, 10)
+	const minutes = Number.parseInt((total / 60) % 60, 10)
+	const seconds = Number.parseInt(total % 60, 10)
+
+	return {
+		total,
+		minutes,
+		seconds,
+	}
+}
+
+function startTimer() {
+	let { total } = timer.remainingTime
+	const endTime = Date.parse(new Date()) + total * 1000
+
+	if (timer.mode === 'pomodoro') timer.sessions++
+
+	mainButton.dataset.action = 'stop'
+	mainButton.textContent = 'stop'
+	mainButton.classList.add('active')
+
+	interval = setInterval(function () {
+		timer.remainingTime = getRemainingTime(endTime)
+		updateClock()
+
+		total = timer.remainingTime.total
+		if (total <= 0) {
+			clearInterval(interval)
+
+			switch (timer.mode) {
+				case 'pomodoro':
+					if (timer.sessions % timer.longBreakInterval === 0) {
+						switchMode('longBreak')
+					} else {
+						switchMode('shortBreak')
+					}
+					break
+				default:
+					switchMode('pomodoro')
+			}
+			startTimer()
+		}
+	}, 1000)
+}
+
+function stopTimer() {
+	clearInterval(interval)
+	mainButton.dataset.action = 'start'
+	mainButton.textContent = 'start'
+	mainButton.classList.remove('active')
+}
+
+function updateClock() {
+	const { remainingTime } = timer
+	const minutes = `${remainingTime.minutes}`.padStart(2, '0')
+	const seconds = `${remainingTime.seconds}`.padStart(2, '0')
+
+	const min = document.getElementById('js-minutes')
+	const sec = document.getElementById('js-seconds')
+	min.textContent = minutes
+	sec.textContent = seconds
+
+	const progress = document.getElementById('js-progress')
+	progress.value = timer[timer.mode] * 60 - timer.remainingTime.total
 }
 
 function switchMode(mode) {
@@ -32,19 +103,22 @@ function switchMode(mode) {
 		.querySelectorAll('button[data-mode]')
 		.forEach(e => e.classList.remove('active'))
 	document.querySelector(`[data-mode="${mode}"]`).classList.add('active')
+	document
+		.getElementById('js-progress')
+		.setAttribute('max', timer.remainingTime.total)
 
 	updateClock()
 }
 
-function updateClock() {
-	const { remainingTime } = timer
-	console.log(remainingTime)
+function handleMode(event) {
+	const { mode } = event.target.dataset
 
-	const minutes = `${remainingTime.minutes}`.padStart(2, '0')
-	const seconds = `${remainingTime.seconds}`.padStart(2, '0')
+	if (!mode) return
 
-	const min = document.getElementById('js-minutes')
-	const sec = document.getElementById('js-seconds')
-	min.textContent = minutes
-	sec.textContent = seconds
+	switchMode(mode)
+	stopTimer()
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+	switchMode('pomodoro')
+})
